bd54810787754fd4a533bc8b50bc082227bebb99
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-02-09 08:03:24 -0600

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Student on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
